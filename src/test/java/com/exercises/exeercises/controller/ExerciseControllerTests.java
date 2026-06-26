daa36ba8749cb571e954c9addf63fb17a39c2074
Eric Atekpani <eric.atekpani@uni-muenster.de>
2026-06-26 16:26:32 +0200

    alle dtos zu record refactor

--- src/test/java/com/exercises/exeercises/controller/ExerciseControllerTests.java
+++ src/test/java/com/exercises/exeercises/controller/ExerciseControllerTests.java
@@ -1,11 +1,6 @@
 package com.exercises.exeercises.controller;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import com.exercises.exeercises.model.Exercise;
-import com.exercises.exeercises.model.dto.ExerciseDTO;
-import com.exercises.exeercises.model.id.ExerciseId;
 
 public class ExerciseControllerTests {
 
@@ -13,16 +8,4 @@ public class ExerciseControllerTests {
     public void setUp() {
     }
 
-    @Test
-    public void exerciseAddTest() {
-        ExerciseDTO exerciseDTO = new ExerciseDTO();
-        exerciseDTO.setBoardId(348L);
-        exerciseDTO.setDescription("Beschreibung");
-        exerciseDTO.setTitle("Titel");
-
-        Exercise exercise = new Exercise();
-        exercise.setId(new ExerciseId(348L, 1));;
-        exercise.setTitle("Titel");
-        exercise.setDescription("Beschreibung");
-    }
 }
