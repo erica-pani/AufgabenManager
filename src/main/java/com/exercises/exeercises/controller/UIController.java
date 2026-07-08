922f1cab726449ea5fee3c9746111cc2e3e6280b
Eric Atekpani <eric.atekpani@uni-muenster.de>
2026-07-08 18:27:09 +0200

    thymeleaf

--- src/main/java/com/exercises/exeercises/controller/UIController.java
+++ src/main/java/com/exercises/exeercises/controller/UIController.java
@@ -2,13 +2,11 @@ package com.exercises.exeercises.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class UIController {
     
-
     @GetMapping("/")
     public String home() {
         return "main";
