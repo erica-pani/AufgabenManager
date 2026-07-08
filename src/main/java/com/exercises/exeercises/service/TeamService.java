d7b0a7f02aff1d9ffb1dd13516f1cf724a3c7acb
„Eric <eric.atekpani@uni-muenster.de>
2026-07-08 10:28:28 +0200

    new unimplemented service

---
+++ src/main/java/com/exercises/exeercises/service/TeamService.java
@@ -0,0 +1,28 @@
+package com.exercises.exeercises.service;
+
+import org.springframework.stereotype.Service;
+
+import com.exercises.exeercises.model.Team;
+import com.exercises.exeercises.model.dto.TeamDTO;
+
+@Service
+public class TeamService {
+
+    public Team saveNewTeam(TeamDTO teamDTO) {
+        return null;    
+    }
+
+    public void addTeamMember() {
+
+    }
+
+    public Team changeTeamName() {
+        return null;
+    }
+
+    public void deleteTeam(){
+        
+    }
+
+
+}
