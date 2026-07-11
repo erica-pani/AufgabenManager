6c91c4218d5b4d2dade67b38c548f6a41e03a91a
Eric Atekpani <eric.atekpani@uni-muenster.de>
2026-07-11 20:36:15 +0200

    minor fix

--- src/main/java/com/exercises/exeercises/controller/mapper/BoardMapper.java
+++ src/main/java/com/exercises/exeercises/controller/mapper/BoardMapper.java
@@ -1,12 +1,16 @@
 package com.exercises.exeercises.controller.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import com.exercises.exeercises.model.Board;
 import com.exercises.exeercises.model.dto.BoardResponseDTO;
 
 @Mapper(componentModel = "spring")
 public interface BoardMapper {
 
-    BoardResponseDTO toDTO();
+    @Mapping(source = "id", target = "boardId")
+    //@Mapping(source = "", target = "")
+    BoardResponseDTO toDTO(Board board);
     
 }
